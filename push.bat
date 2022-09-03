814a0083674f484b0f99cd1d2278f6b895d1fbc7
rrjjyy <wangyiyun163rjy@163.com>
2022-09-03 23:22:32 +0800

    1

--- push.bat
+++ push.bat
@@ -1,11 +1,12 @@
-@echo off
+﻿@echo off
 echo "-------Begin-------"
 git status
 
 git add .
 git commit -m "1"
 git pull
-git push git@github.com:rrjjyy/docsify.git 
+git push git@github.com:rrjjyy/docsify.git git push -u origin main -f
+
 echo 推送成功：【%1%】
 echo "--------End!--------"
 pause
